5e8450acfda070438d6fe08ea26abf36378df572
Gman1996 <dayoagbolade@gmail.com>
2019-05-08 16:22:33 +0100

    Removed some listed routes from router.js

--- src/router.js
+++ src/router.js
@@ -5,9 +5,7 @@ import Map from './components/views/Map'
 
 const routes = [
     { path: '/', component: Home },
-    { path: '/results', component: Results },
     { path: '/latlng', component: FetchLatLng },
-    { path: '/map', component: Map },
 ];
 
 export default routes;
