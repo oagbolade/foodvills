159c80e9f2f09935f77dd4b4408f55c8e7ca42cc
Gman1996 <dayoagbolade@gmail.com>
2019-05-16 16:06:44 +0100

    worked on package.json and searchbar

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 // server.js
-// var express = require('express');
-// var path = require('path');
-// var serveStatic = require('serve-static');
-// let app = express();
-// app.use(serveStatic(__dirname + "/dist"));
-// var port = process.env.PORT || 5000;
-// app.listen(port);
-// console.log('server started '+ port);
+var express = require('express');
+var path = require('path');
+var serveStatic = require('serve-static');
+let app = express();
+app.use(serveStatic(__dirname + "/dist"));
+var port = process.env.PORT || 5000;
+app.listen(port);
+console.log('server started '+ port);
 
 // Testing code
 const express = require('express');
